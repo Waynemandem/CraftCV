425b66cf8ef71286d97fd59374d97e12244f6b26
Big <okoemu4@gmail.com>
2026-07-07 02:05:16 +0100

    fix: remove unnecessary BrowserRouter import and component wrapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import { useEffect }       from 'react'
-import { BrowserRouter, Routes, Route }   from 'react-router-dom'
+import { Routes, Route }   from 'react-router-dom'
 import Landing             from './pages/Landing'
 import Auth                from './pages/Auth'
 import Dashboard           from './pages/Dashboard'
@@ -22,7 +22,6 @@ export default function App() {
   useEffect(() => { init() }, [])
 
   return (
-    <BrowserRouter>
     <Routes>
       <Route path="/"     element={<Landing />}  />
       <Route path="/auth" element={<Auth />}      />
@@ -40,6 +39,5 @@ export default function App() {
       <Route path="/payment-success" element={<PaymentSuccess />}  />
       <Route path="*" element={<NotFound />} />
     </Routes>
-    </BrowserRouter>
   )
 }
